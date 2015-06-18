d333c419ca3484e134a2d9fb34c62bc8651159fc
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2015-06-18 18:55:12 +0530

    Add fixed footer for error page

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,23 @@
 var $ = require('jquery');
 var _ = require('underscore');
+
+$(function() {
+
+  var $html = $('html');
+
+  var timer = setInterval(function() {
+    if ($html.hasClass('wf-active')) {
+      resizeHeight();
+      clearTimeout(timer);
+    }
+  }, 50);
+
+  function resizeHeight() {
+    var $err = $('.site-error');
+
+    $err.height($(window).height() - 138);
+    $err.fadeIn(750, function() {
+      console.log('wheeeee');
+    });
+  }
+});
